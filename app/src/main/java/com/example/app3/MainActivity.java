6ffffc5c5ece4e4800650937e34396b244319f4e
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-01-31 20:59:47 -0500

    New Updates and Files

--- app/src/main/java/com/example/app3/MainActivity.java
+++ app/src/main/java/com/example/app3/MainActivity.java
@@ -2,19 +2,38 @@ package com.example.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button start;
+    Button instructions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.beginGame);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(startIntent);
+            }
+        });
+
+        instructions = findViewById(R.id.instructions);
+
+        instructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent instructionIntent = new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(instructionIntent);
+            }
+        });
     }
 
-    public void onBeginButtonClick(View view){
-
-    }
 
 }
