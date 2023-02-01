783d7099feb220d1caa36d30c90913f788b7a724
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-01-31 21:19:10 -0500

    New Updates

--- app/src/main/java/com/example/app3/MainActivity2.java
+++ app/src/main/java/com/example/app3/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button instructions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        instructions = findViewById(R.id.refresher);
+
+        instructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewInst = new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(viewInst);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/app3/MainActivity3.java
+++ app/src/main/java/com/example/app3/MainActivity3.java
@@ -2,13 +2,25 @@ package com.example.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        back = findViewById(R.id.backHome);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backIntent = new Intent(MainActivity3.this,MainActivity.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
